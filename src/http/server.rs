218ab8d398ffa3536d9660a8caba3a91989f9b37
Jason Healy <jason@healy.app>
2024-04-12 09:59:34 -0400

    move server logic to module

---
+++ src/http/server.rs
@@ -0,0 +1,58 @@
+use super::thread_pool::ThreadPool;
+use std::fs;
+use std::io::{prelude::*, BufReader};
+use std::net::{TcpListener, TcpStream, ToSocketAddrs};
+
+pub fn new<A>(listen_addr: A, workers: usize) -> Result<(), std::io::Error>
+where
+    A: ToSocketAddrs,
+{
+    let listener = TcpListener::bind(listen_addr)?;
+
+    let pool = ThreadPool::new(workers);
+
+    for conn in listener.incoming() {
+        match conn {
+            Ok(stream) => {
+                pool.execute(|| {
+                    match handle_connection(stream) {
+                        Ok(()) => {
+                            println!("connection closed successfully");
+                        }
+                        Err(e) => {
+                            eprintln!("error handling connection: {:?}", e);
+                        }
+                    };
+                });
+            }
+            Err(e) => {
+                eprintln!("error creating connection: {:?}", e);
+            }
+        }
+    }
+    println!("Shutting down.");
+
+    Ok(())
+}
+
+fn handle_connection(mut stream: TcpStream) -> Result<(), std::io::Error> {
+    let buf_reader = BufReader::new(&mut stream);
+    if let Some(request_line) = buf_reader.lines().next() {
+        // TODO: requests are more than a single line
+        // TODO: parse into a request object
+        let (status_line, filename) = match &request_line?[..] {
+            "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+            _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+        };
+
+        let contents = fs::read_to_string(format!("www/{filename}"))?;
+        let length = contents.len();
+
+        // TODO: build response object
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+        stream.write_all(response.as_bytes())?;
+    };
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,11 @@
-mod server;
+mod http;
 
-use server::thread_pool::ThreadPool;
-use std::{
-    fs,
-    io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream},
-};
-
+use http::server;
 const DEFAULT_ADDR_IP4: &str = "127.0.0.1";
 const DEFAULT_PORT: u16 = 7878;
 const DEFAULT_WORKERS: usize = 8;
 
 fn main() -> Result<(), std::io::Error> {
-    // TODO: pull out all of this to a module
-    // ideally it will be called like
-    //
-    // server::register_handler(uri: string, hander: func)
-    // server::listen_and_serve(socket: impl ToSocketAddr)
-    //
-    // ThreadPools don't need to be exposed to main at all
-
-    let socket_ipv4 = format!("{DEFAULT_ADDR_IP4}:{DEFAULT_PORT}");
-    let listener = TcpListener::bind(socket_ipv4)?;
-
-    let pool = ThreadPool::new(DEFAULT_WORKERS);
-
-    for conn in listener.incoming() {
-        match conn {
-            Ok(stream) => {
-                pool.execute(|| {
-                    match handle_connection(stream) {
-                        Ok(()) => {
-                            println!("connection closed successfully");
-                        }
-                        Err(e) => {
-                            eprintln!("error handling connection: {:?}", e);
-                        }
-                    };
-                });
-            }
-            Err(e) => {
-                eprintln!("error creating connection: {:?}", e);
-            }
-        }
-    }
-    println!("Shutting down.");
-
-    Ok(())
-}
-
-fn handle_connection(mut stream: TcpStream) -> Result<(), std::io::Error> {
-    let buf_reader = BufReader::new(&mut stream);
-    if let Some(request_line) = buf_reader.lines().next() {
-        // TODO: requests are more than a single line
-        // TODO: parse into a request object
-        let (status_line, filename) = match &request_line?[..] {
-            "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
-            _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
-        };
-
-        let contents = fs::read_to_string(format!("www/{filename}"))?;
-        let length = contents.len();
-
-        // TODO: build response object
-        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-        stream.write_all(response.as_bytes())?;
-    };
-
-    Ok(())
+    let listen_addr = format!("{DEFAULT_ADDR_IP4}:{DEFAULT_PORT}");
+    server::new(listen_addr, DEFAULT_WORKERS)
 }
