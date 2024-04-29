d740f49a5e7e916fdc3e43cd6250312d7025e1aa
Jason Healy <jason@healy.app>
2024-04-28 22:39:52 -0400

    parse additional request headers

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,4 +1,4 @@
-use std::str::FromStr as _;
+use std::{collections::HashMap, str::FromStr as _};
 
 use super::{constants::HTTP1_1, method::Method};
 
@@ -7,7 +7,7 @@ pub struct Request {
     pub method: Method,
     pub target: String,
     protocol: &'static str,
-    host: String,
+    headers: HashMap<String, String>,
 }
 
 impl Request {
@@ -15,7 +15,6 @@ impl Request {
     where
         T: std::io::BufRead,
     {
-        // BUG: response is served before request has ended (double CRLF)
         let mut lines = req.lines();
 
         let request_line = if let Some(line) = lines.next() {
@@ -23,31 +22,43 @@ impl Request {
         } else {
             return Err(ParseRequestError.into());
         };
-        let [method_raw, target, protocol] = match request_line.split(' ').collect::<Vec<&str>>()[..]
-        {
-            [method, target, protocol] => [method, target, protocol],
+        let [method_raw, target] = match request_line.split(' ').collect::<Vec<&str>>()[..] {
+            [method, target, HTTP1_1] => [method, target],
             _ => return Err(ParseRequestError.into()),
         };
         let method = Method::from_str(method_raw)?;
-        if protocol != HTTP1_1 {
-            return Err(ParseRequestError.into());
-        }
 
+        let mut headers = HashMap::new();
         let host_line = if let Some(line) = lines.next() {
             line?
         } else {
             return Err(ParseRequestError.into());
         };
-        let host = match host_line.split_once(':') {
-            Some(("Host", domain)) => domain.trim(),
-            _ => return Err(ParseRequestError.into()),
+        if let Some(("Host", domain)) = host_line.split_once(':') {
+            headers.insert("Host".into(), domain.trim().into());
+        } else {
+            return Err(ParseRequestError.into());
         };
+
+        for line in lines {
+            match line {
+                Ok(l) => {
+                    if l == "" {
+                        break;
+                    }
+                    if let Some((k, v)) = l.split_once(':') {
+                        headers.insert(k.trim().into(), v.trim().into());
+                    }
+                }
+                Err(e) => return Err(e.into()),
+            }
+        }
 
         Ok(Self {
             method,
-            target: String::from(target),
+            headers,
+            target: target.into(),
             protocol: HTTP1_1,
-            host: String::from(host),
         })
     }
 }
@@ -71,11 +82,13 @@ mod test {
     #[test]
     fn parse_succeeds() {
         let request = Cursor::new("GET / HTTP/1.1\r\nHost: example.com\r\n\r\n".as_bytes());
+        let mut headers = HashMap::new();
+        headers.insert("Host".into(), "example.com".into());
         let want = Request {
             method: Method::GET,
             target: String::from("/"),
             protocol: "HTTP/1.1",
-            host: String::from("example.com"),
+            headers,
         };
 
         let got = Request::parse(request).unwrap();
@@ -85,14 +98,43 @@ mod test {
     #[test]
     fn parse_succeeds_with_port_in_host() {
         let request = Cursor::new("GET / HTTP/1.1\r\nHost: localhost:7878\r\n\r\n".as_bytes());
+        let mut headers = HashMap::new();
+        headers.insert("Host".into(), "localhost:7878".into());
         let want = Request {
             method: Method::GET,
             target: String::from("/"),
             protocol: "HTTP/1.1",
-            host: String::from("localhost:7878"),
+            headers,
         };
         let got = Request::parse(request).unwrap();
         assert_eq!(got, want)
+    }
+
+    #[test]
+    fn parse_additional_headers() {
+        let req_raw = vec![
+            "GET / HTTP/1.1",
+            "Host: example.com",
+            "User-Agent: rust unit test",
+            "foo: bar",
+            "\r\n",
+        ]
+        .join("\r\n");
+        let request = Cursor::new(req_raw.as_bytes());
+
+        let mut headers = HashMap::new();
+        headers.insert("Host".into(), "example.com".into());
+        headers.insert("User-Agent".into(), "rust unit test".into());
+        headers.insert("foo".into(), "bar".into());
+
+        let want = Request {
+            method: Method::GET,
+            protocol: HTTP1_1,
+            target: "/".into(),
+            headers,
+        };
+        let got = Request::parse(request).unwrap();
+        assert_eq!(got, want);
     }
 
     #[test]
